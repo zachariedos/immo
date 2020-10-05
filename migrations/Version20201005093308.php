352b63bb3fb2a42d196eacfc2dde8906368df651
Zacharie <zacharie21000@gmail.com>
2020-10-05 12:38:41 +0200

    Bootstrap et ajout nom prénom User

---
+++ migrations/Version20201005093308.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201005093308 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD nom VARCHAR(36) NOT NULL, ADD prenom VARCHAR(32) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP nom, DROP prenom');
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -19,9 +19,43 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string", length=36)
+     */
+    private $Nom;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $Prenom;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->Prenom;
+    }
+
+    public function setPrenom(string $Prenom): self
+    {
+        $this->Prenom = $Prenom;
+
+        return $this;
     }
 }
