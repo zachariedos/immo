6439697d484c2386b751ba5f8cb626b23bcd0bf0
Zacharie <zacharie21000@gmail.com>
2020-10-06 12:22:29 +0200

    Fonctionnement image complet

---
+++ public/js/changepreviewimg.js
@@ -0,0 +1,11 @@
+$('.vich-image img').css("width", '100%');
+$('.vich-image a').removeAttr("href");
+$("#bien_imageFile_file").next('.custom-file-label').html($('.vich-image img').attr('src').replace('/images/biens/', ''));
+$("#bien_imageFile_file").change(function (e) {
+    let url = URL.createObjectURL(e.target.files[0]);
+    $('.vich-image img').remove();
+    let fileName = e.target.files[0].name;
+    $(this).next('.custom-file-label').html(fileName);
+    $(this).parents('.vich-image').append('<img class="preview_image" src="' + url + '" />');
+    $('.vich-image img').css("width", '100%');
+})
